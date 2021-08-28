bb9a40620059330f33ca9b1b491d6ab7986cb2a2
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2021-08-28 20:53:14 +0530

    object shorthand notation

--- js/app.js
+++ js/app.js
@@ -5,10 +5,8 @@ const updateCity = async (city) => {
     const cityDets = await getCity(city);
     const weather = await getWeather(cityDets.Key);
 
-    return {
-        cityDets: cityDets,
-        weather: weather
-    };
+    // object shorthand notation
+    return { cityDets, weather };
 
 };
 
