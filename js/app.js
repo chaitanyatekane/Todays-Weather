2e6876bd5917a01c059e8be40a820af255854e43
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2021-08-29 09:54:03 +0530

    add weather images/icons and update on UI

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
 
 const updateUI = (data) => {
 
@@ -16,6 +18,19 @@ const updateUI = (data) => {
         <span>&deg;C</span>
     </div>
     `;
+
+    // update the icon images
+    let iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+
+    // update night/day image
+    let timeSrc = null;
+    if(weather.IsDayTime) {
+        timeSrc = 'img/day.svg';
+    } else {
+        timeSrc = 'img/night.svg';
+    }
+    time.setAttribute('src', timeSrc);
 
     // remove the d-none bootstrap class if present
     if(card.classList.contains('d-none')) {
