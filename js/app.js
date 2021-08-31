e2b4e3518fa10b46c0800fa78adb45abcdf8f8da
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2021-08-31 19:28:24 +0530

    add OOP concepts

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const card = document.querySelector('.card');
 const details = document.querySelector('.details');
 const time = document.querySelector('img.time');
 const icon = document.querySelector('.icon img');
+const forecast = new Forecast();
 
 const updateUI = (data) => {
 
@@ -36,16 +37,6 @@ const updateUI = (data) => {
     }
 };
 
-const updateCity = async (city) => {
-    
-    const cityDets = await getCity(city);
-    const weather = await getWeather(cityDets.Key);
-
-    // object shorthand notation
-    return { cityDets, weather };
-
-};
-
 cityForm.addEventListener('submit', e => {
     // prevent default action
     e.preventDefault();
@@ -55,7 +46,7 @@ cityForm.addEventListener('submit', e => {
     cityForm.reset();
 
     // update the UI with new city
-    updateCity(city)
+    forecast.updateCity(city)
         .then(data => updateUI(data))
         .catch(err => console.log(err));
 
@@ -66,7 +57,7 @@ cityForm.addEventListener('submit', e => {
 
 // local storage check
 if(localStorage.getItem('city')) {
-    updateCity(localStorage.getItem('city'))
+    forecast.updateCity(localStorage.getItem('city'))
         .then(data => updateUI(data))
         .catch(err => console.log(err));
 }
